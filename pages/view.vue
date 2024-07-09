64d599ef2db9fb5fc41c61ab82f4f1fd7d529a12
atomflunder <80397293+atomflunder@users.noreply.github.com>
2024-07-09 20:12:41 +0200

    Add redirect

--- pages/view.vue
+++ pages/view.vue
@@ -13,10 +13,15 @@
 // TODO: Display at all
 // TODO: Group them into the different types (700s together, 350 v2s together etc. etc.) with the UDivider + label
 
-// TODO: Query validation.
-
 const query = useRoute().query;
 const title: string = query.title as string;
+
+// TODO: Proper query validation?
+if (!query.items) {
+    console.log(title, query);
+    await navigateTo("/");
+}
+
 const decodedItems = decodeItems(query.items as string);
 </script>
 
